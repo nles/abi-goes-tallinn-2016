8218294badd84148d1ef10217d5a62ecd0519949
deploy <deploy@pea.nu>
2015-09-22 16:27:23 +0300

    latest

--- uploader/server.rb
+++ uploader/server.rb
@@ -1,18 +1,29 @@
 require 'rubygems'
+require 'bundler'
+require 'bundler/setup'
 require 'sinatra'
 require 'haml'
+
+set :bind, '0.0.0.0'
+set :port, 4001 
 
 configure do
   site_root = '../'
   site_assets = site_root+'source/assets/'
   set :site_root, site_root
   set :site_assets, site_assets
+  set :secret, File.read(".secret").split("\n")
 end
 
 # shortcut for calling configs
 module Config
   def self.site_root; return Sinatra::Application.settings.site_root; end
   def self.site_assets; return Sinatra::Application.settings.site_assets; end
+  def self.secret; return Sinatra::Application.settings.secret; end
+end
+
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == Config.secret[0] and password == Config.secret[1]
 end
 
 # Handle GET-request (Show the upload form)
@@ -28,8 +39,10 @@ post "/upload" do
   File.open(target + uploaded[:filename], "w") do |f|
     f.write(uploaded[:tempfile].read)
   end
-  system("#{Config.site_root}bin/jekyll_build.bash")
-  system("#{Config.site_root}bin/s3_push.bash &")
+  Bundler.with_clean_env do
+    system("#{Config.site_root}bin/jekyll_build.bash")
+    #system("#{Config.site_root}bin/s3_push.bash &")
+  end
   redirect "/"
 end
 
